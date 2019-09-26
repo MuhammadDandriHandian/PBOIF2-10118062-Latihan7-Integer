e19e1566776dff485327bcc57125c078dbf821be
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-09-26 14:37:07 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan7-Integer/src/pboif2/pkg10118062/latihan7/integer/PBOIF210118062Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * Nama     : Muhammad Dandri Handian
+ * Kelas    : IF-2
+ * NIM      : 10118062
+ */
+public class PBOIF210118062Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 900000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int: " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
